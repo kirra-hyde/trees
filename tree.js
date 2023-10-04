5e7b9da891e01923c5c7371a3853e17c3e5810a4
Igor Oganesian <igor.oganesian@gmail.com>
2023-10-04 15:22:03 -0400

    trees finished

--- tree.js
+++ tree.js
@@ -11,37 +11,43 @@ class TreeNode {
   /** sumValues(): add up all values of invoking node and its children.
    * Returns sum as an integer. */
   sumValues() {
-    //node. Takes its value.  Go to children
-    if (this.children.length === 0) return 0;
+    // //node. Takes its value.  Go to children
+    let sum = this.val;
 
-    const stack = [this.val];
-    console.log("stack before loop:", stack);
-    console.log("this.children:", this.children);
-    console.log("this.val:", this.val);
-
-    for (child of this.children) {
-      stack.push(child);
+    for (let child of this.children) {
+      sum += child.sumValues();
     }
-    console.log("stack after loop:", stack);
-    let current = stack.pop();
-    console.log("current:", current);
-    return current.val + this.sumValues(stack);
 
-
+    return sum;
   }
 
   /** countEvens(): starting from the invoking node and moving through its
    * children, count how many nodes have even values. Returns that count as
    * an integer. */
   countEvens() {
+    //start at this.val
+      //count # of nodes with this.val % 2 === 0
+      //return int(count)
+    let count = this.val % 2 === 0 ? 1 : 0;
 
+    for (let child of this.children) {
+      count += child.countEvens();
+    }
+
+    return count;
   }
 
   /** numGreater(lowerBound): starting from the invoking node and moving through
    * its children, return a count of the number of nodes whose value is greater
    * than lowerBound. */
   numGreater(lowerBound) {
+    let count = this.val > lowerBound ? 1 : 0;
 
+    for (let child of this.children) {
+      count += child.numGreater(lowerBound);
+    }
+
+    return count;
   }
 }
 
@@ -52,18 +58,30 @@ class Tree {
 
   /** sumValues(): add up all values in the tree. */
   sumValues() {
-
+    if (this.root) {
+      return this.root.sumValues();
+    } else {
+      return 0;
+    }
   }
 
   /** countEvens(): count all nodes in the tree that have even values. */
   countEvens() {
-
+    if (this.root) {
+      return this.root.countEvens();
+    } else {
+      return 0;
+    }
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
   numGreater(lowerBound) {
-
+    if (this.root) {
+      return this.root.numGreater(lowerBound);
+    } else {
+      return 0;
+    }
   }
 }
 
